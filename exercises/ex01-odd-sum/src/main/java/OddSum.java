82bbed45ff2ee7a0b6b0a52b7b418e38307ca319
Kobi Liu <kliu0511@outlook.com>
2026-09-23 14:20:03 -0400

    ex01 comp test

--- exercises/ex01-odd-sum/src/main/java/OddSum.java
+++ exercises/ex01-odd-sum/src/main/java/OddSum.java
@@ -31,6 +31,16 @@ public class OddSum {
         //       Recall arr.length gives the number of elements.
         //       You can index into arrays as we do in Python
         //       (e.g. arr[i] gives you the item at index i).
-        return 0;
+        if (arr.length <= 1){
+            return 0;
+        }
+        else {
+            t = 0;
+            for (int i = 1; i < arr.length; i += 2) {
+                t += arr[i]
+        }
+            return t;
+        }
     }
+
 }
