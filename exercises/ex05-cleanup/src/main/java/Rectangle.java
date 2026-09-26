3744082a8a205bfb3b5c58ea4de9a392ef2fed5a
Kobi Liu <kliu0511@outlook.com>
2026-09-25 20:55:25 -0400

    cleanup ex05 format

--- exercises/ex05-cleanup/src/main/java/Rectangle.java
+++ exercises/ex05-cleanup/src/main/java/Rectangle.java
@@ -1,12 +1,23 @@
+/**
+ * Exercise 05 Chapter X for cleanup design principles, patterns, code-style documentation.
+ *
+ * What does this do
+ *
+ *
+ *
+ *
+ */
 public class Rectangle {
     private double width;
     private double height;
 
+    /** Builds the Rectange with provided arguments w and h **/
     public Rectangle(double w,double h){
         this.width=w;
         this.height=h;
     }
 
+    /** Doubles the area of this rectangle **/
     public double area(){
         return width*height;
     }
@@ -20,10 +31,17 @@ public class Rectangle {
       height = height * factor;
     }
 
+    /**
+     * checks if this rectangle is larger or other rectangle is.
+     * Return true if this rectangle is larger. Otherwise, this rectangle
+     * and is smaller than or equal size other rectangle: return false.
+     * @param other
+     */
     public boolean isLargerThan(Rectangle other){
-        if(area() > other.area())
+        if(area() > other.area()) {
             return true;
-        else
+        } else {
             return false;
+        }
     }
 }
